f58ac829ecc7e5567164ecbac3656fdad4c592d6
Krisztián Fekete <fekete.krisztyan@gmail.com>
2014-05-06 13:30:28 +0200

    add back bad parser & enable it

--- complex_to_psql_tools/drop_invalid_dates.py
+++ complex_to_psql_tools/drop_invalid_dates.py
@@ -1,20 +1,45 @@
 from __future__ import print_function
 from __future__ import unicode_literals
+
 import csv
 # we have some very big multi-line fields going above the default 128 KB limit
 # fortunately they are all still smaller than 1 MB
 csv.field_size_limit(max(1024 ** 2, csv.field_size_limit()))
+
 import sys
 import iso8601
+import time
+
+
+def date_or_None_9940110(datestr):
+    # this version is WRONG, but something like this was used to
+    # this allows 9940110 to pass through, which would fail to load
+    # into postgresql from csv, yet it was surprisingly imported
+    # as 9940-01-10 (rovat_3)
+    #    ceg_id   | alrovat_id |   hattol   | hatig |          nev           |
+    # ------------+------------+------------+-------+------------------------+
+    #  0106313015 |          1 | 9940-01-10 |       | THE PHOENIX "J" "M" BT |
+    # In [1]: import time
+    # In [2]: time.strptime('9940110', '%Y%m%d')
+    # Out[2]: time.struct_time(tm_year=9940, tm_mon=1, tm_mday=10, tm_hour=...
+    # In [3]: time.strftime('%Y-%m-%d', time.strptime('9940110', '%Y%m%d'))
+    # Out[3]: '9940-01-10'
+
+    if datestr:
+        try:
+            parsed = time.strptime(datestr, '%Y%m%d')
+            return time.strftime('%Y-%m-%d', parsed)
+        except ValueError:
+            pass
+
+    return None
 
 
 def date_or_None(datestr):
     if datestr:
         try:
-            iso8601.parse_date(datestr)
-            return datestr
-            # parsed = iso8601.parse_date(datestr)
-            # return parsed.strftime('%Y-%m-%d')
+            parsed = iso8601.parse_date(datestr)
+            return parsed.strftime('%Y-%m-%d')
         except iso8601.ParseError:
             pass
 
@@ -29,7 +54,10 @@ def drop_invalid_dates(fields, csv_reader, csv_writer):
     for row in csv_reader:
         row = list(row)
         for i in date_field_indices:
-            row[i] = date_or_None(row[i])
+            # FIXME: it should use the correct date validator
+            # in the second release - we need the bad version
+            # available for reproducability's sake
+            row[i] = date_or_None_9940110(row[i])
         csv_writer.writerow(row)
 
 
--- complex_to_psql_tools/tests/test_drop_invalid_dates.py
+++ complex_to_psql_tools/tests/test_drop_invalid_dates.py
@@ -7,25 +7,50 @@ import csv
 from complex_to_psql_tools import drop_invalid_dates as m
 
 
-class Test_date_or_None(unittest.TestCase):
+class Test_date_or_None_9940110(unittest.TestCase):
 
-    def test_invalid_date_is_replaced_w_None(self):
-        self.assertIsNone(m.date_or_None('2014-13-41'))
-
-    def test_malformed_date_is_replaced_w_None(self):
-        self.assertIsNone(m.date_or_None('2003/12/12'))
-
-    def test_malformed_date2_is_replaced_w_None(self):
-        self.assertIsNone(m.date_or_None('13-13-13'))
-
-    def test_iso_basic_dates_are_kept(self):
-        self.assertEquals('20140429', m.date_or_None('20140429'))
-
-    def test_iso_extended_dates_are_kept(self):
-        self.assertEquals('2014-04-29', m.date_or_None('2014-04-29'))
+    convert = staticmethod(m.date_or_None_9940110)
 
     def test_malformed_9940110(self):
-        self.assertIsNone(m.date_or_None('9940110'))
+        self.assertEquals('9940-01-10', self.convert('9940110'))
+
+    def test_invalid_date_is_replaced_w_None(self):
+        self.assertIsNone(self.convert('2014-13-41'))
+
+    def test_malformed_date_is_replaced_w_None(self):
+        self.assertIsNone(self.convert('2003/12/12'))
+
+    def test_malformed_date2_is_replaced_w_None(self):
+        self.assertIsNone(self.convert('13-13-13'))
+
+    def test_iso_basic_dates_are_kept(self):
+        self.assertEquals('2014-04-29', self.convert('20140429'))
+
+    def test_iso_extended_dates_are_kept(self):
+        self.assertIsNone(self.convert('2014-04-29'))
+
+
+class Test_date_or_None(unittest.TestCase):
+
+    convert = staticmethod(m.date_or_None)
+
+    def test_invalid_date_is_replaced_w_None(self):
+        self.assertIsNone(self.convert('2014-13-41'))
+
+    def test_malformed_date_is_replaced_w_None(self):
+        self.assertIsNone(self.convert('2003/12/12'))
+
+    def test_malformed_date2_is_replaced_w_None(self):
+        self.assertIsNone(self.convert('13-13-13'))
+
+    def test_iso_basic_dates_are_OK(self):
+        self.assertEquals('2014-04-29', self.convert('20140429'))
+
+    def test_iso_extended_dates_are_OK(self):
+        self.assertEquals('2014-04-29', self.convert('2014-04-29'))
+
+    def test_malformed_9940110_is_replaced_w_None(self):
+        self.assertIsNone(self.convert('9940110'))
 
 
 class Test_drop_invalid_dates(unittest.TestCase):
@@ -49,7 +74,7 @@ class Test_drop_invalid_dates(unittest.TestCase):
             [
                 'a,date1,b,date2,c',
                 'a,,b,,c',
-                'a,20140429,b,20131231,c',
+                'a,2014-04-29,b,2013-12-31,c',
             ],
             unicode(output.getvalue()).splitlines()
         )
